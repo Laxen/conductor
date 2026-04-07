0a3a0f552d493b74ce9e2fbe2e2bbb1a3420d57f
Laxen <1144747+Laxen@users.noreply.github.com>
2026-04-07 14:25:10 +0200

    Update __all__ to exclude SlackIntegration
    Removed SlackIntegration from the exports list.

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -1,6 +1,5 @@
 from integrations.openai import OpenAIIntegration
-from integrations.slack import SlackIntegration
 from integrations.telegram import TelegramIntegration
 from integrations.memory import MemoryApp, MemoryStore
 
-__all__ = ["OpenAIIntegration", "SlackIntegration", "TelegramIntegration", "MemoryApp", "MemoryStore"]
+__all__ = ["OpenAIIntegration", "TelegramIntegration", "MemoryApp", "MemoryStore"]
